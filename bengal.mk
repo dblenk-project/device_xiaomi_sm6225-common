14cf8d9576fecca2c55c8fcc4381c33c35f5e449
Mahesh Sharma <smahesh@codeaurora.org>
2019-12-13 14:35:14 -0800

    sm6225-common: Support new ANT stack
    Add appropriate package name on the basis of
    transport used on target.
    
    Change-Id: I02163a7a50afba8c6ee6a766327b78c5df081d4e

--- bengal.mk
+++ bengal.mk
@@ -81,6 +81,11 @@ PRODUCT_PACKAGES += \
     checkpoint_gc \
     otapreopt_script
 
+# ANT+
+PRODUCT_PACKAGES += \
+    AntHalService-Soong \
+    com.dsi.ant@1.0.vendor
+
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
