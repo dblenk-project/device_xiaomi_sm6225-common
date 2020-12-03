24b10b7a7fd6c2e1395d15e91c22ce70bf98f13a
Alexander Koskovich <zvnexus@outlook.com>
2020-12-03 10:26:15 +0000

    sm6225-common: Disable the usage of ConfigStore.
     * As per commit https://github.com/LineageOS/android_hardware_interfaces/commit/11a113a67e87a8da9a9bf0a73883f2325de90934 ("Add disable_configstore").
    
    Change-Id: I5482989a8a96227a3e2a5317eacf613c641bfa8e

--- bengal.mk
+++ bengal.mk
@@ -169,6 +169,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
 
+# Configstore
+PRODUCT_PACKAGES += \
+    disable_configstore
+
 # Consumer IR
 PRODUCT_PACKAGES += \
     android.hardware.ir@1.0-impl \
