988d52c4d1ab1bb1294f972a753e01946e09c18b
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-02-10 22:10:19 +0100

    sm6225-common: Build Wi-Fi stack
    Change-Id: I2e99bb95cbcdb2fdb652e20bb1e9e55b53cab2eb

--- bengal.mk
+++ bengal.mk
@@ -270,5 +270,17 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.vibrator.service
 
+# WiFi
+PRODUCT_PACKAGES += \
+    android.hardware.wifi@1.0-service \
+    hostapd \
+    libwpa_client \
+    libwifi-hal-ctrl \
+    libwifi-hal-qcom \
+    vendor.qti.hardware.wifi.hostapd@1.2.vendor \
+    vendor.qti.hardware.wifi.supplicant@2.1.vendor \
+    wpa_supplicant \
+    wpa_supplicant.conf
+
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/sm6225-common/sm6225-common-vendor.mk)
