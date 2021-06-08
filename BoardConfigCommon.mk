6def9b6d61a2355c30dee3c17946dedb0d93ed0f
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-06-08 02:17:15 +0200

    sm6225-common: Don't override DEVICE_{MATRIX|MANIFEST}_FILE
    * Allow device-specific manifests to be safely added
    
    Change-Id: Iccafe8c8491219336389015af8caa2c7a700ff61

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -84,8 +84,8 @@ BOARD_HAVE_QCOM_FM := true
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(COMMON_PATH)/framework_compatibility_matrix.xml \
     vendor/lineage/config/device_framework_matrix.xml
-DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
-DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
+DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest.xml
+DEVICE_MATRIX_FILE += $(COMMON_PATH)/compatibility_matrix.xml
 
 # Init
 TARGET_INIT_VENDOR_LIB ?= //$(COMMON_PATH):init_xiaomi_bengal
