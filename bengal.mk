4ca57b077c96a0d2873e486560a0f4f5491635a7
Cosmin Tanislav <demonsingur@gmail.com>
2021-09-21 01:58:57 +0300

    sm6225-common: sensors: Avoid target name conflicts
    Change-Id: If7bd530f2be559f238666a842e9ba5f8e050aeb4

--- bengal.mk
+++ bengal.mk
@@ -397,7 +397,7 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@2.1-service.multihal \
+    android.hardware.sensors@2.1-service.xiaomi_bengal-multihal \
     libsensorndkbridge
 
 # Servicetracker
