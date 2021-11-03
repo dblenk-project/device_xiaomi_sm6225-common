924e777889ae0b41c95d195d92af2bd04c392091
Fabian Leuteneegger <fabian.leutenegger@bluewin.ch>
2021-11-03 17:53:48 +0100

    sm6225-common: Build Neural Networks HIDL
    F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/android.hardware.neuralnetworks@1.3-service-qti": library "android.hardware.neuralnetworks@1.0.so" not found: needed by main executable
    
    Change-Id: Ic6e1ea31f01c13fc6f2fa3dadf4bee672249f295

--- bengal.mk
+++ bengal.mk
@@ -332,6 +332,10 @@ PRODUCT_PACKAGES += \
     android.system.net.netd@1.1.vendor \
     netutils-wrapper-1.0
 
+# Neural Networks
+PRODUCT_PACKAGES += \
+    android.hardware.neuralnetworks@1.3.vendor
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
