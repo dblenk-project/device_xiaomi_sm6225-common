bf1d9d7b9a4c40902d0b63cb56fa0a357f976e37
althafvly <althafvly@gmail.com>
2022-03-24 17:18:43 +0530

    sm6225-common: Inherit launch_with_vendor_ramdisk for vAB
    - fog/spes has vendor_boot partition
    
    Change-Id: I5c344890bc12ee769e2f27369a4151b469271a51

--- bengal.mk
+++ bengal.mk
@@ -14,7 +14,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
 # Enable virtual A/B OTA
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
 
 # AOSP Permissions
 PRODUCT_COPY_FILES += \
