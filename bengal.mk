e398ed1f18b855d990106387afb6fbd1a861e71e
althafvly <althafvly@gmail.com>
2022-03-24 17:07:34 +0530

    sm6225-common: Inherit vAB makefile from new dir
    Change-Id: I10d914c833f460b7b1fab6d357e4b1e255098bac

--- bengal.mk
+++ bengal.mk
@@ -14,7 +14,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
 # Enable virtual A/B OTA
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch.mk)
 
 # AOSP Permissions
 PRODUCT_COPY_FILES += \
