82182aad8dfea97d762577da446d65c54c51eb63
Jabiyeff <cebiyevanar@gmail.com>
2022-05-23 01:26:24 +0400

    sm6225-common: usb: Revert back USB HAL V1.2 version
    * Our device kernel not compatible with V1.3 functions
    
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>
    Change-Id: I67ad09dea623737ad7590848b46b47a6e4df8193

--- bengal.mk
+++ bengal.mk
@@ -444,7 +444,7 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.3-service-xiaomi_bengal \
+    android.hardware.usb@1.2-service-xiaomi_bengal \
     android.hardware.usb.gadget@1.0-service-xiaomi_bengal
 
 # Vendor service manager
