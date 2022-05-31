4e0703d194fde28b9f5270b3c850a010aa09a824
boedhack99 <boedhack@gmail.com>
2022-05-31 16:56:12 +0700

    sm6225-common: Build telephony packages
    Change-Id: I191e0f18b743a79c1b4fe6a58eacd17f205284e5

--- bengal.mk
+++ bengal.mk
@@ -249,5 +249,18 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES_DEBUG += \
     update_engine_client
 
+# Telephony
+PRODUCT_PACKAGES += \
+    ims-ext-common \
+    ims_ext_common.xml \
+    qti-telephony-hidl-wrapper \
+    qti_telephony_hidl_wrapper.xml \
+    qti-telephony-utils \
+    qti_telephony_utils.xml \
+    telephony-ext
+
+PRODUCT_BOOT_JARS += \
+    telephony-ext
+
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/sm6225-common/sm6225-common-vendor.mk)
