4d50654d91cfcadcb7021eab0071d6204cf280ad
boedhack99 <boedhack@gmail.com>
2022-06-03 10:38:59 +0700

    sm6225-common: Import keylayout configs - From spes MIUI V13.0.5.0.RGCMIXM release-keys
    Change-Id: Icf9d7716f76719f83d8be8e128ce6cc33e324c16

--- bengal.mk
+++ bengal.mk
@@ -238,6 +238,12 @@ PRODUCT_PACKAGES += \
     libipanat \
     liboffloadhal
 
+# Keylayout
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/gpio-keys.kl \
+    $(LOCAL_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/uinput-fpc.kl \
+    $(LOCAL_PATH)/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/uinput-goodix.kl
+
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.lights-service
