db719b4fde14c289fe84d0eaeb64e4621e7eaa6a
boedhack99 <boedhack@gmail.com>
2022-06-29 20:35:58 +0700

    sm6225-common: Build camera libs
    Change-Id: Iba74b62341f46cb333d7db29fab0f9a2e9a98cc2

--- bengal.mk
+++ bengal.mk
@@ -65,6 +65,23 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
+# Camera
+PRODUCT_PACKAGES += \
+    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service_64
+
+PRODUCT_PACKAGES += \
+    libcamera2ndk_vendor \
+    libgui_vendor \
+    libstdc++.vendor
+
+PRODUCT_PACKAGES += \
+    Snap
+
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.camera.device@1.0.vendor \
+    vendor.qti.hardware.camera.postproc@1.0.vendor
+
 # Fastbootd
 PRODUCT_PACKAGES += \
     fastbootd
