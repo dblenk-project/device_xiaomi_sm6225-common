cc33b0759876a12446366f013e52649233b7e65f
boedhack99 <boedhack@gmail.com>
2022-06-29 20:48:19 +0700

    sm6225-common: Build health HIDL
    Change-Id: Ifabd56ef2c0898401e4e246fbfeef25e1a478f37

--- bengal.mk
+++ bengal.mk
@@ -158,6 +158,11 @@ PRODUCT_PACKAGES += \
     android.hardware.gnss.measurement_corrections@1.1.vendor \
     android.hardware.gnss.visibility_control@1.0.vendor
 
+# Health
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-service
+
 # Partitions
 PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
