843db00454c96c1b473918f269dcb45d27e615b0
boedhack99 <boedhack@gmail.com>
2022-06-29 22:24:32 +0700

    sm6225-common: Import initial blobs list
    Change-Id: I1191657ac42fe2f64b9858d888762ace7d1e5ff2

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -142,6 +142,9 @@ TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 # RIL
 ENABLE_VENDOR_RIL_SERVICE := true
 
+# Security patch level
+VENDOR_SECURITY_PATCH := 2022-01-01
+
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 
