e724cadc2b602984a72af9009d3cc6d628a5c086
boedhack99 <boedhack@gmail.com>
2022-06-29 16:53:50 +0700

    sm6225-common: Add kernel flags
    Change-Id: Ibc498dbbad8619d812152700752fa9527fd6bcee

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -23,5 +23,32 @@ TARGET_2ND_CPU_VARIANT := cortex-a9
 TARGET_BOOTLOADER_BOARD_NAME := bengal
 TARGET_NO_BOOTLOADER := true
 
+# Kernel
+BOARD_KERNEL_BASE        := 0x00000000
+BOARD_KERNEL_IMAGE_NAME  := Image
+BOARD_KERNEL_OFFSET      := 0x00008000
+BOARD_KERNEL_PAGESIZE    := 4096
+BOARD_RAMDISK_OFFSET     := 0x01000000
+BOARD_TAGS_OFFSET        := 0x00000100
+
+BOARD_BOOT_HEADER_VERSION := 3
+BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
+
+BOARD_KERNEL_CMDLINE += \
+    androidboot.console=ttyMSM0 \
+    androidboot.hardware=qcom \
+    androidboot.memcg=1 \
+    androidboot.usbcontroller=4e00000.dwc3 \
+    console=ttyMSM0,115200n8 \
+    earlycon=msm_geni_serial,0x4a90000 \
+    loop.max_part=7 \
+    lpm_levels.sleep_disabled=1 \
+    msm_rtb.filter=0x237 \
+    service_locator.enable=1 \
+    swiotlb=2048
+
+TARGET_KERNEL_HEADERS := kernel/xiaomi/sm6225-common
+TARGET_KERNEL_SOURCE := kernel/xiaomi/sm6225-common
+
 # Inherit the proprietary files
 include vendor/xiaomi/sm6225-common/BoardConfigVendor.mk
