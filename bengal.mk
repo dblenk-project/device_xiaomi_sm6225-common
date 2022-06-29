966d0c07eac62c5c52ee2f86bea2e77799e3f0ae
boedhack99 <boedhack@gmail.com>
2022-06-29 20:32:35 +0700

    sm6225-common: Build bluetooth libs
    Change-Id: I28f7aa17fd1d02d3c5a7cd3a0bdec3a4ccf70716

--- bengal.mk
+++ bengal.mk
@@ -58,6 +58,13 @@ PRODUCT_PACKAGES += \
     r-gsi.avbpubkey \
     s-gsi.avbpubkey
 
+# Bluetooth
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.bluetooth_audio@2.1.vendor \
+    vendor.qti.hardware.bluetooth_audio@2.1.vendor \
+    vendor.qti.hardware.btconfigstore@1.0.vendor \
+    vendor.qti.hardware.btconfigstore@2.0.vendor
+
 # Fastbootd
 PRODUCT_PACKAGES += \
     fastbootd
