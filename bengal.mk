ada95232c51f64696567e8da8dfd5b90b91bc9d1
boedhack99 <boedhack@gmail.com>
2022-07-01 17:56:31 +0700

    sm6225-common: vibrator: Move excluded-input-devices to vibrator folder section
    Change-Id: Ie2b058a3dfe75e6a22561f95b3e62f4e3932979f

--- bengal.mk
+++ bengal.mk
@@ -449,7 +449,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.vibrator.service.xiaomi_bengal
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
+    $(LOCAL_PATH)/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
 # VNDK
 PRODUCT_EXTRA_VNDK_VERSIONS := 30
