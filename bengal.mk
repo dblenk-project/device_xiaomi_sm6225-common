83c857bc385087da22d41db28587b0b0869dd8c8
itsxrp <itsxrproms@gmail.com>
2022-07-11 06:21:23 +0700

    sm6225-common: Build android.hardware.graphics.composer@2.1 [    5.620680] init: Control message: Could not find 'android.hardware.graphics.composer@2.1::IComposer/default' for ctl.interface_start from pid: 514 (/system/bin/hwservicemanager)
    Signed-off-by: itsxrp <itsxrproms@gmail.com>
    Change-Id: Ifcc80a6a669ae75318c524421cf74b5da49276a3

--- bengal.mk
+++ bengal.mk
@@ -203,6 +203,7 @@ PRODUCT_PACKAGES += \
 
 # Display
 PRODUCT_PACKAGES += \
+    android.hardware.graphics.composer@2.1 \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
     android.hardware.memtrack@1.0-impl \
