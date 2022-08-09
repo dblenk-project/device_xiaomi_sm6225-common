03c766c7c0b3ce8ee2d51e9d6f1716537988d14a
boedhack99 <boedhack@gmail.com>
2022-08-09 20:48:15 +0700

    sm6225-common: Rename custom product overlay * komodo to nad
    Change-Id: I147ddcda4ef10eef0c2d7638ab0722d6e2b0f1d1

--- bengal.mk
+++ bengal.mk
@@ -339,7 +339,7 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-komodo
+    $(LOCAL_PATH)/overlay-nad
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
