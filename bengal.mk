5e84d0219cfaa99d85645a3becbd1c01f14dbd6b
boedhack99 <boedhack@gmail.com>
2022-08-18 10:16:23 +0700

    sm6225-common: keylayout: Import input-silead keylayout * From fog MIUI V13.0.7.0.RGEMIXM * Keep with disable fingerprint keys
    Change-Id: I9b4e92800b4e51659a84f316ff107948b9ae849f

--- bengal.mk
+++ bengal.mk
@@ -279,7 +279,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/gpio-keys.kl \
     $(LOCAL_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/uinput-fpc.kl \
-    $(LOCAL_PATH)/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/uinput-goodix.kl
+    $(LOCAL_PATH)/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/uinput-goodix.kl \
+    $(LOCAL_PATH)/keylayout/uinput-silead.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/uinput-silead.kl
 
 # Keymaster
 PRODUCT_PACKAGES += \
