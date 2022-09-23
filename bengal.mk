2b078debf48d7bdeea4673d06d3282188b84cc80
boedhack99 <boedhack@gmail.com>
2022-09-23 15:47:03 +0700

    sm6225-common: Remove lineage basic feature * Stop build lineage.trust * Drop lineage FCM * Remove the dependencies
    Change-Id: I8b1bdec6af20438af7547134cf001ba885143c9e

--- bengal.mk
+++ bengal.mk
@@ -428,10 +428,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
-# Trust
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
-
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service-qti
