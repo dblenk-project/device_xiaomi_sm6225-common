d7eca568534fb2896e2ef45fec4a28c04e6be752
johnmart19 <johnivan19999@gmail.com>
2022-11-03 21:35:01 +0200

    sm6225-common: Nuke OTA Preopt Script
    - OTA Dexopt optimizations taking a lot of time, let's skip it on installation moment
    
    Change-Id: I7f674c41b23b60ead39e5695f24f78eb256bd9ed

--- bengal.mk
+++ bengal.mk
@@ -59,20 +59,13 @@ PRODUCT_COPY_FILES += \
 
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
-    RUN_POSTINSTALL_system=true \
-    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
-    FILESYSTEM_TYPE_system=ext4 \
-    POSTINSTALL_OPTIONAL_system=true
-
-AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_vendor=true \
     POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
     FILESYSTEM_TYPE_vendor=ext4 \
     POSTINSTALL_OPTIONAL_vendor=true
 
 PRODUCT_PACKAGES += \
-    checkpoint_gc \
-    otapreopt_script
+    checkpoint_gc
 
 # ANT+
 PRODUCT_PACKAGES += \
