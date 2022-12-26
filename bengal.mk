237b64b1979cf89cfaac0d9d6bba89b0944334ad
Alexander Koskovich <akoskovich@pm.me>
2022-12-26 17:07:10 +0700

    sm6225-common: Update to Boot Control HAL v1.2
    Change-Id: If29f5699050479e3e1100374e467a61a75e9065a

--- bengal.mk
+++ bengal.mk
@@ -121,9 +121,9 @@ PRODUCT_PACKAGES += \
 
 # Bootcontrol
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.1-impl-qti \
-    android.hardware.boot@1.1-impl-qti.recovery \
-    android.hardware.boot@1.1-service
+    android.hardware.boot@1.2-impl-qti \
+    android.hardware.boot@1.2-impl-qti.recovery \
+    android.hardware.boot@1.2-service
 
 PRODUCT_PACKAGES_DEBUG += \
     bootctl
